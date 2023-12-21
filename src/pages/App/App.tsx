41310e4cd8f2adcf11743cf20df14f3153600a73
Matteo Altamore <mat.altamore@gmail.com>
2023-12-21 21:41:39 +0100

    feat: core page layout

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,9 +1,20 @@
 import "./App.module.scss";
 import Filmography from "../Filmography/Filmography";
 import Timeline from "../Timeline/Timeline";
+import styles from "./App.module.scss";
 
 function App() {
-  return <Timeline />;
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>Studio Ghibli</header>
+      <main className={styles.main}>
+        <Filmography />
+      </main>
+      <aside className={styles.aside}>
+        <Timeline />
+      </aside>
+    </div>
+  );
 }
 
 export default App;
